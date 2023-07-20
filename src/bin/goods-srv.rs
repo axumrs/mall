c9ca84cb733abf1f51ce596b443df8dadc692966
axumrs <rolson867@gmail.com>
2023-07-20 17:45:53 +0800

    品牌：服务

---
+++ src/bin/goods-srv.rs
@@ -0,0 +1,26 @@
+use mall::{
+    config::ServerConfig, etcd, pb::goods_service_server::GoodsServiceServer, srv::goods_srv,
+};
+
+#[tokio::main]
+async fn main() {
+    let cli = etcd::conn().await.unwrap();
+    let cfg: Option<ServerConfig> = etcd::get(&cli, "/goods-srv").await.unwrap();
+    let cfg = cfg.unwrap_or_default();
+
+    println!("{:?}", cfg);
+
+    let pool = sqlx::mysql::MySqlPoolOptions::new()
+        .connect(&cfg.db_dsn)
+        .await
+        .unwrap();
+    let sf = snowflake::SnowflakeIdGenerator::new(cfg.node.machine_id, cfg.node.node_id);
+
+    let srv = goods_srv::Goods::new(pool, sf);
+
+    tonic::transport::Server::builder()
+        .add_service(GoodsServiceServer::new(srv))
+        .serve((&cfg.addr).parse().unwrap())
+        .await
+        .unwrap();
+}
