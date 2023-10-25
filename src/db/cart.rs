433e1e928c6da24aeef3f48833a447270a0bf463
axumrs <rolson867@gmail.com>
2023-10-25 14:20:30 +0800

    购物车：数据库操作单元测试

--- src/db/cart.rs
+++ src/db/cart.rs
@@ -1,5 +1,4 @@
 use crate::{model, utils};
-use futures::StreamExt;
 
 /// 添加到购物车
 pub async fn add_item<'a>(
@@ -60,7 +59,7 @@ pub async fn remove_item<'a>(
         b.push_bind(id);
     });
 
-    q.push(" RETURNING *");
+    println!("sql: {}", q.sql());
 
     q.build_query_as().fetch_all(e).await
 }
@@ -77,4 +76,151 @@ pub async fn update_num<'a>(
         .await?
         .rows_affected();
     Ok(aff)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::model;
+
+    async fn get_conn() -> sqlx::PgPool {
+        sqlx::postgres::PgPoolOptions::new()
+            .max_connections(5)
+            .connect("postgres://mall:mall@127.0.0.1:5432/mall")
+            .await
+            .unwrap()
+    }
+    #[tokio::test]
+    async fn test_db_add_item_cart() {
+        let conn = get_conn().await;
+        let mut tx = conn.begin().await.unwrap();
+
+        let c = model::Cart {
+            user_id: format!("{}", "cj7kfuel6bcqn8bicrng"),
+            goods_id: format!("{}", "ckmivtsdrfagh6slh3tg"),
+            // goods_sku: format!("{}", "米色-M-V领"),
+            goods_sku: format!("{}", "米色-S-V领"),
+            num: model::U32::new(1),
+            // amount: model::U32::from_yuan(100),
+            dateline: chrono::Local::now(),
+            ..Default::default()
+        };
+
+        let id = match super::add_item(&mut *tx, &c).await {
+            Ok(id) => id,
+            Err(err) => {
+                tx.rollback().await.unwrap();
+                panic!("{:?}", err);
+            }
+        };
+
+        let ls = match super::get(&mut *tx, &c.user_id).await {
+            Ok(ls) => ls,
+            Err(err) => {
+                tx.rollback().await.unwrap();
+                panic!("{:?}", err);
+            }
+        };
+
+        tx.commit().await.unwrap();
+
+        println!("id: {}, ls: {:?}", id, ls);
+    }
+
+    #[tokio::test]
+    async fn test_db_get_items_cart() {
+        let conn = get_conn().await;
+        let mut tx = conn.begin().await.unwrap();
+        let ls = match super::get(&mut *tx, "cj7kfuel6bcqn8bicrng").await {
+            Ok(ls) => ls,
+            Err(err) => {
+                tx.rollback().await.unwrap();
+                panic!("{:?}", err);
+            }
+        };
+        tx.commit().await.unwrap();
+        println!("{:?}", ls)
+    }
+
+    #[tokio::test]
+    async fn test_db_update_num_cart() {
+        let conn = get_conn().await;
+        let mut tx = conn.begin().await.unwrap();
+
+        let items = vec![model::CartItemNum {
+            id: "cksauc4drfao8bsjqhsg".to_string(),
+            num: model::U32::new(10),
+        }];
+
+        let mut affs = Vec::with_capacity(items.len());
+        for i in items.iter() {
+            let aff = match super::update_num(&mut *tx, i).await {
+                Ok(aff) => aff,
+                Err(err) => {
+                    tx.rollback().await.unwrap();
+                    panic!("{:?}", err);
+                }
+            };
+            affs.push(aff);
+        }
+
+        let ls = match super::get(&mut *tx, "cj7kfuel6bcqn8bicrng").await {
+            Ok(ls) => ls,
+            Err(err) => {
+                tx.rollback().await.unwrap();
+                panic!("{:?}", err);
+            }
+        };
+
+        tx.commit().await.unwrap();
+
+        println!("affs: {:?}, items: {:?}", affs, ls);
+    }
+
+    #[tokio::test]
+    async fn test_db_remove_item_cart() {
+        let conn = get_conn().await;
+        let mut tx = conn.begin().await.unwrap();
+
+        let r = model::RemoveItemFromCartRequest {
+            ids: vec!["cksauc4drfao8bsjqhsg".to_string()],
+            user_id: "cj7kfuel6bcqn8bicrng".to_string(),
+        };
+
+        let removted = match super::remove_item(&mut *tx, &r).await {
+            Ok(rm) => rm,
+            Err(err) => {
+                tx.rollback().await.unwrap();
+                panic!("{:?}", err);
+            }
+        };
+
+        let ls = match super::get(&mut *tx, &r.user_id).await {
+            Ok(ls) => ls,
+            Err(err) => {
+                tx.rollback().await.unwrap();
+                panic!("{:?}", err);
+            }
+        };
+
+        tx.commit().await.unwrap();
+        println!("remoted: {:?}, ls:{:?}", removted, ls)
+    }
+
+    #[tokio::test]
+    async fn test_db_clear_item_cart() {
+        let conn = get_conn().await;
+        let mut tx = conn.begin().await.unwrap();
+
+        let aff = match super::clear(&mut *tx, "cj7kfuel6bcqn8bicrng").await {
+            Ok(a) => a,
+            Err(err) => {
+                tx.rollback().await.unwrap();
+                panic!("{:?}", err);
+            }
+        };
+
+        tx.commit().await.unwrap();
+
+        println!("aff: {}", aff);
+    }
 }
